ad6784341d85a04c719dd56902ddc96b841f6ec1
Kate Donaldson <kate@katelovescode.com>
2021-02-10 08:50:43 -0600

    Initial terraform test

---
+++ main.tf
@@ -0,0 +1,24 @@
+# provider
+provider "aws" {
+  region = "us-east-1"
+  profile = "pie"
+}
+
+# S3 resources
+resource "aws_kms_key" "encryption_key" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
+resource "aws_s3_bucket" "pie" {
+  bucket = "mybucket"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.mykey.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+}
